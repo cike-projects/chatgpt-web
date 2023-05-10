bed95e8c3db13a4c15736c0ae8a1f22104237b23
zhangyunan <zyndev@gmail.com>
2023-05-10 22:35:57 +0800

    Merge branch 'main' into feat_sidebar

--- src/views/main/MainLayout.vue
+++ src/views/main/MainLayout.vue
@@ -3,8 +3,10 @@ import { computed, ref } from 'vue'
 import { useBasicLayout } from '@/hooks/useBasicLayout'
 import { HoverButton, Setting, SimpleUserAvatar, SvgIcon } from '@/components/common'
 import {useRouter} from "vue-router";
-import {useMessage} from "naive-ui";
+import {useMessage, useNotification } from "naive-ui";
 const message = useMessage()
+const notification = useNotification()
+
 const { isMobile } = useBasicLayout()
 
 const getMobileClass = computed(() => {
@@ -27,7 +29,10 @@ const gotoRoute = function (route_name: string) {
         <SvgIcon style="font-size: 30px; cursor: pointer;" icon="solar:chat-dots-outline" @click="gotoRoute('Chat')" />
       </div>
       <div class="sidebar-x">
-        <SvgIcon style="font-size: 40px; cursor: pointer;" icon="skill-icons:mysql-dark" @click="message.error('暂未实现')" />
+        <SvgIcon style="font-size: 40px; cursor: pointer;" icon="skill-icons:mysql-dark" @click="notification.warning({title:'duration: 10000', content: '暂未实现', duration: 10000})" />
+      </div>
+      <div class="sidebar-x">
+        <SvgIcon style="font-size: 40px; cursor: pointer;" icon="solar:code-bold-duotone" @click="notification.warning({title:'duration: 10000', content: '暂未实现', duration: 10000})" />
       </div>
     </div>
     <div style="position:fixed; bottom: 20px">
